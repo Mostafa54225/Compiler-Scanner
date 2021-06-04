4f83a9cd6bb495d2312f64006bd38ff50fdfcbb5
Mostafa54225 <mostafa.draven@gmail.com>
2021-06-04 15:12:04 +0200

    different classes created

---
+++ Scanner/Scanner/SyntaxToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scanner
+{
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+        }
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text { get; }
+    }
+}
